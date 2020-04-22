010ecd802977aa5a3253b28c79d18fef168b62f0
SebastianHafner <hafnersailing@gmail.com>
2020-04-22 11:57:29 +0200

    cloud and drive export functions for generic download

--- export.py
+++ export.py
@@ -1,12 +1,48 @@
 import ee
+import utils
 
 
-def to_drive(img: ee.Image, bbox: ee.Geometry, folder: str, file_name: str, patch_size: int = 0,
-             scale: int = 10, crs: str = 'EPSG:4326'):
-    if patch_size == 0:
+def construct_task(cfg, data):
+
+    task = None
+    if cfg.DOWNLOAD.TYPE == 'drive':
+        # exporting an image if no sampling was applied
+        if cfg.SAMPLING.TYPE == 'none':
+            task = image_to_drive(
+                img=data,
+                roi=utils.extract_bbox(cfg),
+                folder=cfg.DOWNLOAD.DRIVE_FOLDER,
+                file_name=cfg.BBOX.ID,
+                patch_size=cfg.SAMPLING.PATCH_SIZE
+            )
+        # if image was sampled exporting a feature collection
+        else:
+            task = table_to_drive(fc=data, properties=['buildingPercentage', 'B2', 'B3', 'B4', 'B8'],
+                                  folder=cfg.DOWNLOAD.DRIVE_FOLDER, file_name=cfg.BBOX.ID)
+
+    if cfg.DOWNLOAD.TYPE == 'cloud' and False:
+        pass
+
+    return task
+
+
+def table_to_drive(fc: ee.FeatureCollection, folder: str, file_name: str):
+    task = ee.batch.Export.table.toDrive(
+        collection=fc,
+        description='PythonToDriveExport',
+        folder=folder,
+        fileNamePrefix=file_name,
+        fileFormat='GeoJSON'
+    )
+    return task
+
+
+def image_to_drive(img: ee.Image, roi: ee.Geometry, folder: str, file_name: str, patch_size: int = -1,
+                   scale: int = 10, crs: str = 'EPSG:4326'):
+    if patch_size == -1:
         task = ee.batch.Export.image.toDrive(
             image=img,
-            region=bbox.getInfo()['coordinates'],
+            region=roi.getInfo()['coordinates'],
             description='PythonToDriveExport',
             folder=folder,
             fileNamePrefix=file_name,
@@ -18,7 +54,7 @@ def to_drive(img: ee.Image, bbox: ee.Geometry, folder: str, file_name: str, patc
     else:
         task = ee.batch.Export.image.toDrive(
             image=img,
-            region=bbox.getInfo()['coordinates'],
+            region=roi.getInfo()['coordinates'],
             description='PythonToDriveExport',
             folder=folder,
             fileNamePrefix=file_name,
@@ -28,13 +64,13 @@ def to_drive(img: ee.Image, bbox: ee.Geometry, folder: str, file_name: str, patc
             fileDimensions=patch_size,
             fileFormat='GeoTIFF'
         )
-    task.start()
+    return task
 
 
-def to_cloud(img: ee.Image, bbox: ee.Geometry, bucket: str, folder: str, file_name: str, patch_size: int = 0,
-             scale: int = 10, crs: str = 'EPSG:4326'):
+def image_to_cloud(img: ee.Image, bbox: ee.Geometry, bucket: str, folder: str, file_name: str, patch_size: int = -1,
+                   scale: int = 10, crs: str = 'EPSG:4326'):
 
-    if patch_size == 0:
+    if patch_size == -1:
         task = ee.batch.Export.image.toCloudStorage(
             image=img,
             region=bbox.getInfo()['coordinates'],
@@ -59,4 +95,4 @@ def to_cloud(img: ee.Image, bbox: ee.Geometry, bucket: str, folder: str, file_na
             fileDimensions=patch_size,
             fileFormat='GeoTIFF'
         )
-    task.start()
+    return task
